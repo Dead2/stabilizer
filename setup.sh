0ffaceb20ab0e1a93adcc231b72304bc656716f5
Tim Adye <Tim.Adye@cern.ch>
2021-06-30 21:36:24 +0100

    split setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,3 @@
-unset PLATFORM
 . /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_99 x86_64-centos7-clang10-opt
 #. /cvmfs/sft.cern.ch/lcg/releases/clang/10.0.0/x86_64-centos7/setup.sh
-export INCLUDE_DIRS="$BASE/include"
-export STABILIZER=$(dirname "$BASH_SOURCE")
-export PATH="$STABILIZER:$PATH"
-export LD_LIBRARY_PATH="$STABILIZER:$LD_LIBRARY_PATH"
+. $(dirname "$BASH_SOURCE")/setup-base.sh
