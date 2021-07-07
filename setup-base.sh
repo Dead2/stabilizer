e02d2f8730751c70c27cb887843a9658708d0a47
magras <magras@users.noreply.github.com>
2021-07-07 12:52:55 +0300

    remove setup files

--- setup-base.sh
+++
@@ -1,6 +0,0 @@
-unset PLATFORM
-#. /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_99 x86_64-centos7-clang10-opt
-export INCLUDE_DIRS="$BASE/include"
-export STABILIZER=$(dirname $(readlink -f "$BASH_SOURCE"))
-export PATH="$STABILIZER:$PATH"
-export LD_LIBRARY_PATH="$STABILIZER:$LD_LIBRARY_PATH"
--- setup.sh
+++
@@ -1,3 +0,0 @@
-. /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_99 x86_64-centos7-clang10-opt
-#. /cvmfs/sft.cern.ch/lcg/releases/clang/10.0.0/x86_64-centos7/setup.sh
-. $(dirname "$BASH_SOURCE")/setup-base.sh
